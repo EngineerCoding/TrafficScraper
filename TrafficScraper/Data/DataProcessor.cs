88168a4ed354fc7f4d3fb5724a1e2d7df65c653a
EngineerCoding <engineercoding@hotmail.nl>
2019-09-05 17:00:05 +0200

    Implement main processor

---
+++ TrafficScraper/Data/DataProcessor.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace TrafficScraper.Data
+{
+    class DataProcessor
+    {
+        private readonly DataReader reader;
+        private readonly DataWriter writer;
+        private bool processed = false;
+
+        public DataProcessor(DataReader reader, DataWriter writer)
+        {
+            this.reader = reader;
+            this.writer = writer;
+        }
+
+        public void Process()
+        {
+            if (processed) return;
+            processed = true;
+
+            List<TrafficJam> trafficJams = reader.FetchTrafficJams();
+            writer.WriteTrafficJams(trafficJams);
+        }
+
+    }
+}
