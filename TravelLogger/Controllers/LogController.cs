739f2be90ee13746cbfdc45738d652c32eb2525e
EngineerCoding <engineercoding@hotmail.nl>
2019-09-08 02:47:15 +0200

    Start working on TravelLogger application

---
+++ TravelLogger/Controllers/LogController.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using TravelLogger.Models;
+
+namespace TravelLogger.Controllers
+{
+    public class LogController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult StartLog()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel {RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier});
+        }
+    }
+}
---
+++ TravelLogger/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TravelLogger.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
